73237bd787d340b5bfb1bee8597cbb3cc443d41f
Céline Renault <renault.celine@icloud.com>
2022-08-25 08:41:20 +0200

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+// console.log('hello');
+let btn = document.getElementById('btn-photo');
+let photoChien = document.getElementById('chien-photo');
+
+btn.addEventListener('click', afficher)
+
+function afficher() {
+    fetch('https://dog.ceo/api/breeds/image/random')
+    .then(function (response) {
+        return response.json();
+    }).then(function(data){
+            photoChien.innerHTML =`<img src="${data.message}"/>`;
+        })
+}
